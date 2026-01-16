86082596a59f1fd78adbc9c37ff8854b5af6245c
marllus <roger.mo.leal@gmail.com>
2026-01-16 15:01:09 -0300

    feat(aula-11): adiciona o menu mobile ao site

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+/* Menu de Navegação */
+
 let navBar = document.querySelector("#header");
 
 document.addEventListener("scroll", () => {
@@ -8,4 +10,20 @@ document.addEventListener("scroll", () => {
   } else {
     navBar.classList.remove("rolar");
   }
+});
+
+/* Menu Mobile */
+
+let btnMenuMob = document.querySelector("#btn-menu-mob");
+let line1 = document.querySelector(".line-menu-mob-1");
+let line2 = document.querySelector(".line-menu-mob-2");
+let menuMobile = document.querySelector("#menu-mobile");
+
+let body = document.querySelector("body");
+
+btnMenuMob.addEventListener("click", () => {
+  line1.classList.toggle("ativo1");
+  line2.classList.toggle("ativo2");
+  menuMobile.classList.toggle("abrir");
+  body.classList.toggle("no-overflow");
 });
